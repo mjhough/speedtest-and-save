3b652dfbc753370cd0150fac58da992e1a98d951
Matt Hough <matt@hough.tv>
2017-12-06 08:27:19 +1000

    Write to file in same directory as executing file

--- speed_test_and_save.py
+++ speed_test_and_save.py
@@ -1,5 +1,9 @@
-import speedtest
 from datetime import datetime
+import os
+import inspect
+import speedtest
+
+dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 servers = []
 
@@ -10,5 +14,5 @@ s.get_best_server()
 download = round(s.download() / 1000000, 2)
 upload = round(s.upload() / 1000000, 2)
 
-f = open("speedtests.txt", "a")
+f = open(dir + "/speedtests.txt", "a")
 f.write("Date: " + str(datetime.now()) + " | Download: " + str(download) + "mbps" + " | Upload: " + str(upload) + "mbps\n")
